106b4ecd91b27ff30b85b6bb360fd654efb4decb
jaceanderson <jaceanderson@gmail.com>
2016-09-15 21:55:36 -0400

    More php testing

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
   require 'functions.php';
+  
+  $conn = connect($config);
+  if ($conn) {
+    $connectMsg = 'We connected!';
+  } else {
+    $connectMsg = 'Doh! No connect';
+  }
 ?>
 
 <!doctype html>
@@ -9,6 +16,7 @@
   </head>
   <body>
     <h1>This Here is a Test Page</h1>
-    <h3><?php echo $msg ?></h3>
+    <h3>MSG: <?php echo $msg ?></h3>
+    <h3>connectMsg: <?php echo $connectMsg ?></h3>
   </body>
 </html>
