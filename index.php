de458fe3ee66e20669d3599ce0dc5e722aaa4f56
jaceanderson <jaceanderson@gmail.com>
2016-09-17 23:09:04 -0400

    Added bootstrap, changed db table name

--- index.php
+++ index.php
@@ -12,11 +12,23 @@
 <!doctype html>
 <html>
   <head>
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </head>
   <body>
-    <h1>This Here is a Test Page</h1>
-    <h3>MSG: <?php echo $msg ?></h3>
-    <h3>connectMsg: <?php echo $connectMsg ?></h3>
+    <div class="container-fluid">
+      <h1>This Here is a Test Page</h1>
+      <h3>MSG: <?php echo $msg ?></h3>
+      <h3>connectMsg: <?php echo $connectMsg ?></h3>
+      
+    </div><!--end container-fluid-->
   </body>
 </html>
